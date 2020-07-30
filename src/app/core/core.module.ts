b08dbc1444c698654d1c1f2f6a173747af3f4e19
Dawid Fulmes <fulmes.dawid@gmail.com>
2020-07-30 21:49:07 +0200

    Install ngrx and create root-store

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RootStoreModule } from './store/root-store.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RootStoreModule],
+})
+export class CoreModule {}
---
+++ src/app/core/store/root-store.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { reducers, metaReducers } from './reducers';
+import { environment } from '@environments/environment';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
+})
+export class RootStoreModule {}
