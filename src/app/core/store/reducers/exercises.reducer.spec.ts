69f50deed62ed2d6a7922cb4d13725dec0a13473
Dawid Fulmes <fulmes.dawid@gmail.com>
2020-08-02 19:58:20 +0200

    Write store tests

--- src/app/core/store/reducers/exercises.reducer.spec.ts
+++ src/app/core/store/reducers/exercises.reducer.spec.ts
@@ -1,13 +1,22 @@
+import { ExercisesActions } from '@app/core/store/actions';
 import { exercisesReducer, initialState } from './exercises.reducer';
+import { Exercise } from '@app/core/models';
 
 describe('Exercises Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
-
       const result = exercisesReducer(initialState, action);
-
       expect(result).toBe(initialState);
+    });
+  });
+
+  describe('ExercisesActions.loadExercisesSuccess', () => {
+    it('should append new exercise to state', () => {
+      const exercise: Exercise = { id: 1, description: '', name: '', image: '' };
+      const action = ExercisesActions.loadExercisesSuccess({ exercise });
+      const result = exercisesReducer(initialState, action);
+      expect(result).toEqual({ ...initialState, [exercise.id]: exercise });
     });
   });
 });
--- src/app/core/store/selectors/exercises.selectors.spec.ts
+++ src/app/core/store/selectors/exercises.selectors.spec.ts
@@ -1,7 +1,14 @@
-
+import { selectExerciseById } from '.';
 
 describe('Exercises Selectors', () => {
-  it('should select the feature state', () => {
-    
+  describe('selectExerciseById', () => {
+    it('should return exercise by id', () => {
+      const exercises = {
+        1: { id: 1, description: '', name: '', image: '' },
+        2: { id: 2, description: '', name: '', image: '' },
+      };
+      const result = selectExerciseById.projector(exercises, 1);
+      expect(result).toBe(exercises[1]);
+    });
   });
 });
