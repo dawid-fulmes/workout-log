405964f307f9903457651a461b016c8be0d371ea
Dawid Fulmes <fulmes.dawid@gmail.com>
2020-08-08 19:57:57 +0200

    Add input styling

---
+++ src/app/exercise/components/app-exercise-browser-layout/app-exercise-browser-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-exercise-browser-layout',
+  templateUrl: './app-exercise-browser-layout.component.html',
+  styleUrls: ['./app-exercise-browser-layout.component.scss'],
+})
+export class AppExerciseBrowserLayoutComponent {}
--- src/app/exercise/components/index.ts
+++ src/app/exercise/components/index.ts
@@ -2,12 +2,14 @@ import { ExerciseSuggestionListComponent } from './exercise-suggestion-list/exer
 import { ExerciseOverviewLayoutComponent } from './exercise-overview-layout/exercise-overview-layout.component';
 import { ExerciseOverviewHeaderComponent } from './exercise-overview-header/exercise-overview-header.component';
 import { ExerciseOverviewContentComponent } from './exercise-overview-content/exercise-overview-content.component';
+import { AppExerciseBrowserLayoutComponent } from './app-exercise-browser-layout/app-exercise-browser-layout.component';
 
 const components = [
   ExerciseSuggestionListComponent,
   ExerciseOverviewLayoutComponent,
   ExerciseOverviewHeaderComponent,
   ExerciseOverviewContentComponent,
+  AppExerciseBrowserLayoutComponent,
 ];
 
 export {
@@ -16,4 +18,5 @@ export {
   ExerciseOverviewLayoutComponent,
   ExerciseOverviewHeaderComponent,
   ExerciseOverviewContentComponent,
+  AppExerciseBrowserLayoutComponent,
 };
